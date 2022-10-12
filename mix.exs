d2d734563e888741b459b890a84b2ae5c78affa2
hanzy1110 <eloser1994@gmail.com>
2022-10-12 08:45:23 -0300

    slight

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule IslandsInterface.MixProject do
   def application do
     [
       mod: {IslandsInterface.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :islands_engine]
     ]
   end
 
@@ -45,7 +45,8 @@ defmodule IslandsInterface.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:islands_engine, path: "/home/blueman69/git/IslandEngine/"}
     ]
   end
 
